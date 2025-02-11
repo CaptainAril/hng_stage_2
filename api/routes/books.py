fa0b715b1cb7df7f0ecc12e2ffa38da8268485fb
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2025-02-12 00:46:04 +0100

    test: testing github action workflow

--- api/routes/books.py
+++ api/routes/books.py
@@ -19,17 +19,17 @@ db.books = {
     2: Book(
         id=2,
         title="The Lord of the Rings",
-        author="J.R.R. Tolkien",
+        author="J.R. Tolkien",
         publication_year=1954,
         genre=Genre.FANTASY,
     ),
-    3: Book(
-        id=3,
-        title="The Return of the King",
-        author="J.R.R. Tolkien",
-        publication_year=1955,
-        genre=Genre.FANTASY,
-    ),
+    # 3: Book(
+    #     id=3,
+    #     title="The Return of the King",
+    #     author="J.R.R. Tolkien",
+    #     publication_year=1955,
+    #     genre=Genre.FANTASY,
+    # ),
 }
 
 
@@ -48,12 +48,12 @@ async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
 
 
-@router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
-async def update_book(book_id: int, book: Book) -> Book:
-    return JSONResponse(
-        status_code=status.HTTP_200_OK,
-        content=db.update_book(book_id, book).model_dump(),
-    )
+# @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+# async def update_book(book_id: int, book: Book) -> Book:
+#     return JSONResponse(
+#         status_code=status.HTTP_200_OK,
+#         content=db.update_book(book_id, book).model_dump(),
+#     )
 
 
 @router.delete("/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
